25efea2841ac4d85fac29509c152aba80ee393b0
Erfan Yekehzare <erfan.yekezare@gmail.com>
2023-08-21 15:35:57 +0330

    get users interacted polls and recommend based on those polls

--- Flask/main.py
+++ Flask/main.py
@@ -7,37 +7,38 @@ app = Flask(__name__)
 # @app.route("/get_recom", methods=["POST"])
 @app.route("/get_recom")
 def recom():
-    # 1.Get list from the ElasticSearch query TODO
+    # 1.Get list from the ElasticSearch query TODO. ---------------------------------------------
     polls = get_polls_list("/data/polls_synthetic.csv")
 
-    # 2.encode topics to column
+    # 2.encode topics to column. ---------------------------------------------
     polls = encode_topics(polls)
 
-    # 3.check column datatype
+    # 3.check each column's datatype. ---------------------------------------------
     # check_column_type(polls, "topic", str)
     # check_column_type(polls, "option", str)
     # check_column_type(polls, "author_ID", int)
 
-    # 4.Calculate TF-IDF matrix
+    # 4. get the list of polls, which the user has interacted with. ---------------------------------------------
+    user_interacted_polls = polls.sample(n=10)["poll_ID"]
+
+    # 4.Calculate TF-IDF matrix for [Polls-to-Polls] or [Polls-to-UserProfile]
     polls_tf_idf_matrix = create_tf_idf_matrix(polls, "title")
     cosine_similarity_matrix = calc_cosine_similarity_matrix(
         polls_tf_idf_matrix, polls_tf_idf_matrix
     )
 
-    liked_poll_title = "Do you think cryptocurrency is the future of finance?"
-    liked_poll_index = idx_from_title(polls, liked_poll_title)
+    # liked_poll_title = "Do you think cryptocurrency is the future of finance?"
+    # liked_poll_index = idx_from_title(polls, liked_poll_title)
 
     liked_polls = []
-    recommended_list = gen_recommendations(
-        liked_poll_index, polls, cosine_similarity_matrix, 10
+    recommended_list = gen_rec_from_list_of_polls(
+        user_interacted_polls, polls, cosine_similarity_matrix, 10
     )
     # print(f"recommended_list: [{recommended_list}]")
 
     # print(f"liked poll: [{liked_poll_title}] \nrecommended polls: \n{recommended_list}")
     # ---------------------------------------
 
-    # NEXT TASKs ---------------------------------------------
-    # user_data = request.get_json("user_id")
     # ---------------------------------------
 
     # return jsonify({"recommended_polls": recommended_list})
