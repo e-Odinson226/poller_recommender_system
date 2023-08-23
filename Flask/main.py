649a045161aad95299ac6a91dfb0a79aad5dc723
Erfan Yekehzare <erfan.yekezare@gmail.com>
2023-08-23 08:56:13 +0330

    implement algorithm in REST-api structure recommender.v1

--- Flask/main.py
+++ Flask/main.py
@@ -1,54 +1,38 @@
 from flask import Flask, redirect, url_for, jsonify, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource
 import json
 
-from rec_sys import *
+import rec_sys as rs
 
 app = Flask(__name__)
 api = Api(app)
 
 
 class Rec(Resource):
-    def __init__(self):
-        self.parser = reqparse.RequestParser(bundle_errors=True)
-        # Define the expected argument(s)
-
-        self.parser.add_argument(
-            "user_ID", type=str, required=True, help="User ID is required"
-        )
-        self.parser.add_argument(
-            "interactions",
-            # type=arrayType,
-            type=list,
-            required=True,
-            location="json",
-            action="append",
-            help="Interactions list is required",
-        )
-
-        # self.parser.add_argument("user_ID", type=int, help="Description of param1")
-        # self.parser.add_argument("interactions", type=list, action="append")
+    def __init__(self) -> None:
+        self.polls = rs.get_polls_list("/data/polls_synthetic.csv")
+        self.liked_polls = []
 
     def post(self):
-        args = self.parser.parse_args()
-
+        args = request.get_json(force=True)
         user_id = args.get("user_ID")
         interactions = args.get("interactions")
-        # interactions = args["interactions"]
-        # interactions = json.loads(args["interactions"])
 
-        print(f"user_ID: {user_id}")
-        print(f"Interactions:{interactions}")
+        self.polls = rs.encode_topics(self.polls)
+        polls_tf_idf_matrix = rs.create_tf_idf_matrix(self.polls, "topic")
+        cosine_similarity_matrix = rs.calc_cosine_similarity_matrix(
+            polls_tf_idf_matrix, polls_tf_idf_matrix
+        )
 
-        # for interaction in interactions:
-        #    print(interaction)
+        self.recommended_list = rs.gen_rec_from_list_of_polls(
+            [dic["poll_ID"] for dic in interactions],
+            self.polls,
+            cosine_similarity_matrix,
+            10,
+        )
 
-        result = {"user_id": user_id, "interactions": interactions}
+        result = {"user_id": user_id, "recommended polls": self.recommended_list}
         return result, 200
-
-        # user_id = args["user_ID"]
-        ## interactions = args["interactions"]
-        # interactions = args.get("interactions")
 
 
 api.add_resource(Rec, "/get_rec/")
