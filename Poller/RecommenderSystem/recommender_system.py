2ec92aa05fb6d904cf5bac8881e98cd5e5e70d49
Erfan Yekehzare <erfan.yekezare@gmail.com>
2023-09-14 12:10:03 +0330

    Merge branch 'vastTfIdf' into dev

--- Poller/RecommenderSystem/recommender_system.py
+++ Poller/RecommenderSystem/recommender_system.py
@@ -78,20 +78,6 @@ def create_tf_idf_matrix(df, column):
     return tf_idf.fit_transform(df[column])
 
 
-<<<<<<< HEAD
-# def create_tf_idf_matrix(df, columns):
-#    tf_idf = TfidfVectorizer(stop_words="english")
-#    for column in columns:
-#        # print(f"{df[column]} is {df[column].dtype} and {df[column].dtype is list} {list}: ")
-#        df[column] = df[column].apply(lambda x: " ".join(x))
-#        df[column] = df[column].apply(preprocess_text)
-#
-#    return tf_idf.fit_transform(df[column])
-
-
-def create_soup(df, features):
-    return df["question"] + " " + " ".join(df["options"]) + " " + " ".join(df["topics"])
-=======
 def create_souped_tf_idf_matrix(df):
     df["topics"] = df["topics"].apply(preprocess_list)
     df["question"] = df["question"].apply(preprocess_text)
@@ -112,7 +98,6 @@ def create_soup(df):
     )
     # print(f"-----------------------------------\n* Processing: [{ }]")
     return res
->>>>>>> vastTfIdf
 
 
 def calc_cosine_similarity_matrix(tf_idf_matrix_1, tf_idf_matrix_2):
